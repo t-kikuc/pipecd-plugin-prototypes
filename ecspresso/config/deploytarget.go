d107570426f442955225281f40cd2cffbc816916
t-kikuc <tkikuchi07f@gmail.com>
2025-01-16 10:35:15 +0900

    add initial config
    Signed-off-by: t-kikuc <tkikuchi07f@gmail.com>

---
+++ ecspresso/config/deploytarget.go
@@ -0,0 +1,37 @@
+// Copyright 2024 The PipeCD Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package config
+
+import (
+	"encoding/json"
+	"fmt"
+
+	pipedconfig "github.com/pipe-cd/pipecd/pkg/configv1"
+)
+
+// EcspressoDeployTargetConfig represents PipedDeployTarget.Config for ecspresso plugin.
+type EcspressoDeployTargetConfig struct {
+	// TODO: Add fields if needed.
+}
+
+func ParseDeployTargetConfig(deployTarget pipedconfig.PipedDeployTarget) (EcspressoDeployTargetConfig, error) {
+	var cfg EcspressoDeployTargetConfig
+
+	if err := json.Unmarshal(deployTarget.Config, &cfg); err != nil {
+		return EcspressoDeployTargetConfig{}, fmt.Errorf("failed to unmarshal deploy target configuration: %w", err)
+	}
+
+	return cfg, nil
+}
