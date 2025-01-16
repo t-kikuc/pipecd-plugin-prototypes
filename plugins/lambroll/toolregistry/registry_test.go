2c4b705bd60edd6fe1da20cb46e444d063ca3894
t-kikuc <tkikuchi07f@gmail.com>
2025-01-17 02:00:16 +0900

    Copy ecspresso to lambroll
    Signed-off-by: t-kikuc <tkikuchi07f@gmail.com>

---
+++ plugins/lambroll/toolregistry/registry_test.go
@@ -0,0 +1,48 @@
+// Copyright 2024 The PipeCD Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package toolregistry
+
+import (
+	"context"
+	"os/exec"
+	"strings"
+	"testing"
+
+	"github.com/pipe-cd/pipecd/pkg/plugin/toolregistry/toolregistrytest"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestRegistry_Ecspresso(t *testing.T) {
+	t.Parallel()
+
+	c, err := toolregistrytest.NewToolRegistry(t)
+	require.NoError(t, err)
+
+	r := NewRegistry(c)
+
+	t.Cleanup(func() { c.Close() })
+
+	p, err := r.Ecspresso(context.Background(), "2.4.5")
+	require.NoError(t, err)
+	require.NotEmpty(t, p)
+
+	out, err := exec.CommandContext(context.Background(), p, "version").CombinedOutput()
+	require.NoError(t, err)
+
+	expected := "ecspresso v2.4.5"
+
+	assert.Equal(t, strings.TrimSpace(expected), strings.TrimSpace(string(out)))
+}
