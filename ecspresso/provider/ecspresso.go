e9b47ff776652b8971d9870567c16a7eda26b426
t-kikuc <tkikuchi07f@gmail.com>
2025-01-16 11:09:50 +0900

    add provider.ecspresso
    Signed-off-by: t-kikuc <tkikuchi07f@gmail.com>

---
+++ ecspresso/provider/ecspresso.go
@@ -0,0 +1,78 @@
+// Copyright 2024 The PipeCD Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package provider
+
+import (
+	"context"
+	"fmt"
+	"io"
+	"os/exec"
+	"strings"
+)
+
+type options struct {
+}
+
+type Option func(*options)
+
+type Ecspresso struct {
+	execPath   string
+	dir        string
+	configPath string
+
+	options options
+}
+
+func NewEcspresso(execPath, dir, configPath string, opts ...Option) *Ecspresso {
+	opt := options{}
+	for _, o := range opts {
+		o(&opt)
+	}
+
+	return &Ecspresso{
+		execPath:   execPath,
+		dir:        dir,
+		configPath: configPath,
+		options:    opt,
+	}
+}
+
+func (e *Ecspresso) Version(ctx context.Context) (string, error) {
+	args := []string{"version"}
+	cmd := exec.CommandContext(ctx, e.execPath, args...)
+	cmd.Dir = e.dir
+
+	out, err := cmd.CombinedOutput()
+	if err != nil {
+		return string(out), err
+	}
+
+	return strings.TrimSpace(string(out)), nil
+}
+
+func (e *Ecspresso) Deploy(ctx context.Context, w io.Writer) error {
+	args := []string{
+		"deploy",
+		"--config", e.configPath,
+	}
+
+	cmd := exec.CommandContext(ctx, e.execPath, args...)
+	cmd.Dir = e.dir
+	cmd.Stdout = w
+	cmd.Stderr = w
+
+	io.WriteString(w, fmt.Sprintf("ecspresso %s", strings.Join(args, " ")))
+	return cmd.Run()
+}
