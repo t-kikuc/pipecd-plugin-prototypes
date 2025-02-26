740737350c553640df169238843d3c3bc4531ab1
t-kikuc <tkikuchi07f@gmail.com>
2025-02-27 01:01:16 +0900

    remove _cdk
    Signed-off-by: t-kikuc <tkikuchi07f@gmail.com>

--- plugins/cdk_/toolregistry/scripts.go
+++
@@ -1,8 +0,0 @@
-package toolregistry
-
-const installScript = `
-cd {{ .TmpDir }}
-curl -L https://github.com/fujiwara/lambroll/releases/download/{{ .Version }}/lambroll_{{ .Version }}_{{ .Os }}_{{ .Arch }}.tar.gz -o lambroll_{{ .Version }}_{{ .Os }}_{{ .Arch }}.tar.gz
-tar -zxvf lambroll_{{ .Version }}_{{ .Os }}_{{ .Arch }}.tar.gz
-mv lambroll {{ .OutPath }}
-`
