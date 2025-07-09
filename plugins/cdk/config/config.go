13a2711250d68689919caa5988ef9ff9a0426743
t-kikuc <tkikuchi07f@gmail.com>
2025-07-09 18:09:40 +0900

    rename config (remove CDK prefix)
    Signed-off-by: t-kikuc <tkikuchi07f@gmail.com>

--- plugins/cdk/config/config.go
+++ plugins/cdk/config/config.go
@@ -1,38 +1,54 @@
 package config
 
+import "fmt"
+
 // Config represents the plugin-scoped configuration.
 // type Config struct{}
 
-// CDKDeployTargetConfig represents PipedDeployTarget.Config for cdk plugin.
-type CDKDeployTargetConfig struct {
-	// Version is the version of cdk to use. e.g. "2.1001.0"
-	Version string `json:"version"`
+// DeployTargetConfig represents PipedDeployTarget.Config for cdk plugin.
+type DeployTargetConfig struct {
+	// CDKVersion is the version of cdk to use. e.g. "2.1001.0"
+	// This field is required.
+	CDKVersion string `json:"version"`
 	// NodeVersion is the version of node to use. e.g. "v22.14.0"
+	// This field is required.
 	NodeVersion string `json:"nodeVersion"`
-	// Region is the AWS region to deploy to. e.g. "us-east-1"
-	Region string `json:"region"`
 	// Profile is the AWS profile to use. e.g. "my-profile"
+	// This field is required.
 	Profile string `json:"profile"`
 
 	// TODO: Add fields if needed.
 }
 
-// CDKApplicationSpec represents an application configuration for cdk application.
-type CDKApplicationSpec struct {
-	// Input for cdk deployment. e.g. cdk version
-	Input CDKDeploymentInput `json:"input"`
-	// Configuration for quick sync.
-	QuickSync CDKDeployStageOptions `json:"quickSync"`
+func (c *DeployTargetConfig) Validate() error {
+	if c.CDKVersion == "" {
+		return fmt.Errorf("version is required")
+	}
+	if c.NodeVersion == "" {
+		return fmt.Errorf("nodeVersion is required")
+	}
+	if c.Profile == "" {
+		return fmt.Errorf("profile is required")
+	}
+	return nil
 }
 
-func (s *CDKApplicationSpec) Validate() error {
+// ApplicationSpec represents an application configuration for cdk application.
+type ApplicationSpec struct {
+	// Input for cdk deployment. e.g. cdk version
+	Input DeploymentInput `json:"input"`
+	// Configuration for quick sync.
+	QuickSync DeployStageOptions `json:"quickSync"`
+}
+
+func (s *ApplicationSpec) Validate() error {
 	if err := s.Input.validate(); err != nil {
 		return err
 	}
 	return nil
 }
 
-type CDKDeploymentInput struct {
+type DeploymentInput struct {
 	// Stacks is the list of stacks to deploy.
 	// If you want to deploy all stacks, set "--all".
 	Stacks []string `json:"stacks"`
@@ -41,16 +57,16 @@ type CDKDeploymentInput struct {
 	Contexts []string `json:"contexts"`
 }
 
-func (i *CDKDeploymentInput) validate() error {
+func (i *DeploymentInput) validate() error {
 	return nil
 }
 
-// CDKDeployStageOptions contains all configurable values for a CDK_SYNC stage.
-type CDKDeployStageOptions struct {
+// DeployStageOptions contains all configurable values for a CDK_SYNC stage.
+type DeployStageOptions struct {
 }
 
-// CDKDiffStageOptions contains all configurable values for a CDK_PLAN stage.
-type CDKDiffStageOptions struct {
+// DiffStageOptions contains all configurable values for a CDK_PLAN stage.
+type DiffStageOptions struct {
 	// Exit the pipeline if the result is "No Changes" with success status.
 	// ExitOnNoChanges bool `json:"exitOnNoChanges"`
 }
